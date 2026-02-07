52b81e55acc01a12539a9cd69167c7c83eafaa4d
sirij3 <siriyadalam@gmail.com>
2026-02-07 16:08:29 +0530

    Add files via upload

---
+++ POPCORN7.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+		int x =s.nextInt();
+		System.out.println((x-100)/50);
+
+	}
+}
