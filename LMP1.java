d9d9bde85e10700edc447a2a7ec9ef5258ce4a9f
sirij3 <siriyadalam@gmail.com>
2026-02-07 16:22:18 +0530

    Add files via upload

---
+++ LMP1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s= new Scanner(System.in);
+		int x =s.nextInt();
+		int y =s.nextInt();
+		if(x>=y)
+		System.out.println("Yes");
+		else
+		System.out.println("No");
+
+	}
+}
